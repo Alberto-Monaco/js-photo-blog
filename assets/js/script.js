84d403063f052a4ff526a458d23380f77ca4a21d
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-18 12:01:46 +0200

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,11 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 //?Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata*/
+
+const rowEl = document.querySelector('.row')
+axios
+	.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+
+	.then((response) => {})
+
+	.catch((error) => console.log(error))
